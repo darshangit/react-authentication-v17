034cb58c7f23e462c560f65494b756d87d5e2f15
Darshan Satya <darshans1989@gmail.com>
2021-05-18 21:02:29 -0400

    password change

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,14 +1,41 @@
+import { useContext, useRef } from "react";
+import { FIREBASE_API_KEY } from "../../firebase";
+import AuthContext from "../../store/auth-context";
+
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+const newPasswordInputRef = useRef();
+const authContext = useContext(AuthContext);
+
+const submitHandler = event => {
+  event.preventDefault();
+
+  const enteredNewPassword = newPasswordInputRef.current.value;
+
+  fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key='+FIREBASE_API_KEY, {
+    method: 'POST',
+    body: JSON.stringify({
+      idToken: authContext.token,
+      password: enteredNewPassword,
+      returnSecureToken: false
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(res => {
+    alert("password change - wohoo")
+  })
+}
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' minLength="6" ref={newPasswordInputRef}/>
       </div>
       <div className={classes.action}>
-        <button>Change Password</button>
+        <button type="submit">Change Password</button>
       </div>
     </form>
   );
